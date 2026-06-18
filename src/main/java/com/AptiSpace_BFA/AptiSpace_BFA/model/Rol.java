a45d5fba126da156d5770a20879aff8f3881fa0c
zepedaG06 <jazepeda@uamv.edu.ni>
2026-06-18 12:32:17 -0600

    Crear Usuario y Rol

---
+++ src/main/java/com/AptiSpace_BFA/AptiSpace_BFA/model/Rol.java
@@ -0,0 +1,27 @@
+package com.AptiSpace_BFA.AptiSpace_BFA.model;
+
+import javax.persistence.*;
+
+import org.openxava.annotations.*;
+import org.openxava.model.*;
+
+import lombok.*;
+
+@Entity
+@Getter @Setter
+@View(members=
+	"Datos generales { nombre, activo; descripcion }"
+)
+@Tab(properties="nombre, descripcion, activo")
+public class Rol extends Identifiable {
+
+	@Column(length=40, unique=true)
+	@Required
+	String nombre;
+
+	@Column(length=150)
+	String descripcion;
+
+	boolean activo = true;
+
+}
