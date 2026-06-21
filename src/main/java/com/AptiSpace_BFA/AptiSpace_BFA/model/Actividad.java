1bcf7745531f05937bb8b3c90abb4ecfb1eaac3a
Diego052508 <dfarmengol@uamv.edu.ni>
2026-06-21 14:36:22 -0600

    Implementacion de la entidad actividad

--- src/main/java/com/AptiSpace_BFA/AptiSpace_BFA/model/Actividad.java
+++ src/main/java/com/AptiSpace_BFA/AptiSpace_BFA/model/Actividad.java
@@ -1,4 +1,37 @@
 package com.AptiSpace_BFA.AptiSpace_BFA.model;
 
+import java.time.*;
+import javax.persistence.*;
+import org.openxava.annotations.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@View(members =
+        "nombre;" +
+                "descripcion;" +
+                "fechaEntrega;" +
+                "seccion"
+)
 public class Actividad {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 100)
+    @Required
+    private String nombre;
+
+    @Stereotype("MEMO")
+    private String descripcion;
+
+    @Required
+    private LocalDate fechaEntrega;
+
+    @ManyToOne
+    @DescriptionsList
+    @Required
+    private Seccion seccion;
 }
