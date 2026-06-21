224579f76552fc164938338b195e99cc4a506e64
Diego052508 <dfarmengol@uamv.edu.ni>
2026-06-21 15:40:26 -0600

    Correcion de la entidad Evaluacion

--- src/main/java/com/AptiSpace_BFA/AptiSpace_BFA/model/Evaluacion.java
+++ src/main/java/com/AptiSpace_BFA/AptiSpace_BFA/model/Evaluacion.java
@@ -1,4 +1,31 @@
 package com.AptiSpace_BFA.AptiSpace_BFA.model;
 
+import java.time.*;
+import javax.persistence.*;
+import org.openxava.annotations.*;
+import lombok.*;
+
+@Entity
+@Getter @Setter
 public class Evaluacion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @ReadOnly
+    Long id;
+
+    @Required
+    @Column(length = 100)
+    String nombre;
+
+    @Required
+    Double porcentaje;
+
+    @Required
+    LocalDate fecha;
+
+    @ManyToOne
+    @DescriptionsList
+    @Required
+    Seccion seccion;
 }
