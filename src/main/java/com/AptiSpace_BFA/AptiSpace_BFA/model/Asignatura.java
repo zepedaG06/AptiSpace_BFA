3abdd12a692f8ba948f9f1a25ba2dae39602eb24
Elias <ejmatusg@uamv.edu.ni>
2026-06-21 20:28:10 -0600

    se agrega modelo "Asignatura, Curso y propiedades"

---
+++ src/main/java/com/AptiSpace_BFA/AptiSpace_BFA/model/Asignatura.java
@@ -0,0 +1,41 @@
+package com.AptiSpace_BFA.AptiSpace_BFA.model;
+
+import java.util.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.*;
+
+import org.openxava.annotations.*;
+import org.openxava.model.*;
+
+import lombok.*;
+
+@Entity
+@Getter @Setter
+@View(members=
+        "Datos de asignatura { codigo, nombre; creditos; descripcion } " +
+                "Cursos { cursos }"
+)
+@Tab(properties="codigo, nombre, creditos")
+public class Asignatura extends Identifiable {
+
+    @Column(length=20, unique=true)
+    @Required
+    String codigo;
+
+    @Column(length=80)
+    @Required
+    String nombre;
+
+    @Min(1)
+    @Max(10)
+    int creditos = 1;
+
+    @Column(length=250)
+    String descripcion;
+
+    @OneToMany(mappedBy="asignatura")
+    @ListProperties("codigo, nombre")
+    Collection<Curso> cursos = new ArrayList<>();
+
+}
